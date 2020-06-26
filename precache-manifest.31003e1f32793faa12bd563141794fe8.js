006cbf75bb8d898944f6a952f098db4bfad22d84
SyedMunawwar78 <49419441+SyedMunawwar78@users.noreply.github.com>
2020-06-26 11:54:20 -0700

    initial

---
+++ precache-manifest.31003e1f32793faa12bd563141794fe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab052517628635190e7c20a9974cb5da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "992b18853294f1a51cb7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb2a1f7d84f0637e709b",
+    "url": "/static/js/2.65870548.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.65870548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992b18853294f1a51cb7",
+    "url": "/static/js/main.431cdb17.chunk.js"
+  },
+  {
+    "revision": "4d8d778dd5ccfc0d68b2",
+    "url": "/static/js/runtime-main.49c07d5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
